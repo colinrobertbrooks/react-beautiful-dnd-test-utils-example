af180270d58e4cb1888f5954f7cf9c229a43b5a6
Colin Cummings <colin_cummings@brown.edu>
2019-05-09 13:34:06 -0400

    working board

---
+++ README.md
@@ -0,0 +1,27 @@
+# react-beautiful-dnd-test-utils-example
+
+A fork of the `working-board` branch of [react-beautiful-dnd-task-app](https://github.com/alexreardon/react-beautiful-dnd-task-app).
+
+---
+
+## Setup
+
+Run `npm install`.
+
+---
+
+## Run the Tas App
+
+Run `npm start` and open [localhost:3000](http://localhost:3000/).
+
+---
+
+## Tests
+
+Run `npm test`.
+
+---
+
+## Boilerplate
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/Task.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? 'lightgreen' : 'white')};
+`;
+
+export default class Task extends React.Component {
+  render() {
+    console.log('rendering task', this.props.task.id);
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            innerRef={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
+  }
+}
